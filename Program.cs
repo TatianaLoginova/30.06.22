b0cbd2138816cf853c832c58473e3826281fd745
pandora <pandora_tl@yahoo.com>
2022-06-30 12:42:25 +0300

    задача3

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a, x;
+
+Console.Write("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+x = a % 2;
+
+//Console.Write(x);
+
+if(x == 1 | x == -1){
+    Console.Write("Число не является четным и делится на 2 с остатком");    
+}else
+{
+    Console.Write("Число является четным и делится на 2 без остатка");    
+}
